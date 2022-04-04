ee32ffbba7303ad46cfdfecce2ea56e65ace6068
marcodeiulio <mdeiulio@icloud.com>
2022-04-04 15:18:23 +0200

    - installed & updated composer

--- public/js/app.js
+++ public/js/app.js
@@ -53783,8 +53783,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/reidar/dev/laravel/march/march_30th/laravel-many-to-many/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/reidar/dev/laravel/march/march_30th/laravel-many-to-many/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/reidar/dev/laravel/april/april_4th/laravel-boolpress/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/reidar/dev/laravel/april/april_4th/laravel-boolpress/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
