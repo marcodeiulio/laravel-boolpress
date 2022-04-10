7c64fb6c4b20cd48e87cc512728abbcf478ee6c0
marcodeiulio <mdeiulio@icloud.com>
2022-04-10 17:11:40 +0200

    api now sends contact-mail when called

--- app/Http/Controllers/Api/ContactMailController.php
+++ app/Http/Controllers/Api/ContactMailController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMail;
 
 class ContactMailController extends Controller
 {
     public function send(Request $request)
     {
         $data = $request->all();
-        return response()->json($data);
+
+        $mail = new ContactMail($data);
+        Mail::to(env('MAIL_ADDRESS'))->send($mail);
+
+        return response('Mail sent', 204);
     }
 }
